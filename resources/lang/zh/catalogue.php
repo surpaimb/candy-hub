d20ad9d6893830ecab0dc2d4bab8ddca84cc0480
youshui <16181814@qq.com>
2018-11-16 23:43:17 +0800

    更新翻译

--- resources/lang/zh/catalogue.php
+++ resources/lang/zh/catalogue.php
@@ -1,20 +1,20 @@
 <?php
 
 return [
-    'title'   => 'Catalogue Manager',
+    'title'   => '目录管理器',
     'products' => '商品列表',
     'categories' => '商品分类',
-    'collections'=>'Collections',
-    'productFamilies' => 'Product Families',
-    'save' => 'Save',
+    'collections'=>'收藏',
+    'productFamilies' => '产品库',
+    'save' => '保存',
     'attributes' => '商品属性',
     'attributeGroups'=>'属性分组',
     'editAttributeGroup' => '编辑属性分组',
     'editAttribute' => '编辑属性',
     'editCategory' => '编辑分类',
     'editProduct' => '编辑商品',
-    'editProductFamily' => 'Edit Product Family',
-    'editCollection' => 'Edit Collection',
+    'editProductFamily' => '编辑产品库',
+    'editCollection' => '编辑收藏',
     'settings'=>'设置',
 
 ];
